8a7f21989f83c32ce88ff34f9e929c8b8d8f1677
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-11-28 00:03:38 +0530

    [mrunal] Add. Added project to send mail to active employees in company

---
+++ EmpPayroll.sql
@@ -0,0 +1,15 @@
+create table EmployeeData(
+Name varchar(50),
+Age varchar(10),
+Address varchar(100),
+Role varchar(50),
+Salary varchar(20),
+Phone varchar(20),
+Email varchar(30),
+Active_Inactive varchar(20),
+Attachment varchar(150)
+);
+
+select * from [dbo].[EmployeeData];
+
+delete from  [dbo].[EmployeeData];
